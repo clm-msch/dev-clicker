4b30baace5445210764ad865638584d3d223ee7b
clement <58054176+clm-msch@users.noreply.github.com>
2023-03-05 03:37:26 +0100

    add functionnality and design testing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     css: [
-        'bootstrap/dist/css/bootstrap.css'
+        'bootstrap/dist/css/bootstrap.css',
+        'animate.css/animate.min.css'
     ]
 })
