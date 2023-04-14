c03f0e854ce969b2b96323f2073cfe8b280490b4
clement <58054176+clm-msch@users.noreply.github.com>
2023-04-14 17:54:11 +0200

    add pwa

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,40 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA(
+      {
+        manifest: {
+          icons: [
+            {
+            src: '/icons/512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+            },
+          ],
+        },
+        workbox: {
+          runtimeCaching: [
+            {
+              urlPattern: ({ url }) =>  {
+                return url.pathname.startsWith('/api')
+              },
+              handler: "CacheFirst",
+              options: {
+                cacheName: "api-cache",
+                cacheableResponse: {
+                  statuses: [0, 200],
+                },
+              },
+            }],
+        },
+      }
+    )
+  ],
 })
 
