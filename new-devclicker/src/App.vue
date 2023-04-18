bb8791c16ab96492a846ab8e7b628366cbd4d920
clement <58054176+clm-msch@users.noreply.github.com>
2023-04-18 21:54:22 +0200

    add mobile bar

--- new-devclicker/src/App.vue
+++ new-devclicker/src/App.vue
@@ -1,7 +1,19 @@
 <template>
+	<!-- Bar -->
+  <MobileBar />
+  <div class="flex">
   <DesktopBar />
+	<!-- Bar -->
+  </div>
+
+
 </template>
 
 <script setup>
-  import DesktopBar from './components/Desktop-bar.vue'
+/*
+  import components
+*/
+  import DesktopBar from "./components/Desktop-bar.vue"
+  import MobileBar from "./components/Mobile-bar.vue"
+
 </script>
--- new-devclicker/src/components/Desktop-bar.vue
+++ new-devclicker/src/components/Desktop-bar.vue
@@ -1,46 +1,52 @@
+<!-- Mise en place du conmposant sidebar uniquement pour les utilisateurs sur desktop, elle s'adapte autant pour le plein écran que pour une demi page. -->
 <template>
-		<div class="h-screen rounded-r-lg bg-dark_grey gap-2 flex flex-col p-4 w-fit">
-			<div class="justify-center flex">
-				<img src="../assets/icons/devclicker/devclicker-text.svg" class="w-40 md:w-52">
-			</div>
-			<h2 class="text-white font-bold mt-2 -mb-2">Junior :</h2>
-			<div class="bg-primary p-2 rounded-lg flex items-center gap-4 w">
-				<img src="../assets/icons/basics/code.svg" class="w-6" />
-				<p class="font-bold text-white">Code</p>
-			</div>
-			<div class="bg-primary p-2 rounded-lg flex items-center gap-4 w">
-				<img src="../assets/icons/basics/missions.svg" class="w-6" />
-				<p class="font-bold text-white">Missions</p>
-			</div>
-			<h2 class="text-white font-bold mt-2 -mb-2">Senior :</h2>
-			<div class="bg-primary p-2 rounded-lg flex items-center gap-4 w">
-				<img src="../assets/icons/basics/investissement.svg" class="w-6" />
-				<p class="font-bold text-white">Investissement</p>
-			</div>
-			<div class="bg-primary p-2 rounded-lg flex items-center gap-4 w">
-				<img src="../assets/icons/basics/entreprise.svg" class="w-6" />
-				<p class="font-bold text-white">Entreprise</p>
-			</div>
-			<div class="bg-primary p-2 rounded-lg flex items-center gap-4 w">
-				<img src="../assets/icons/basics/cybersécurité.svg" class="w-6" />
-				<p class="font-bold text-white">Cybersécurité</p>
-			</div>
-			<h2 class="text-white font-bold mt-2 -mb-2">Autres :</h2>
-			<div class="bg-primary p-2 rounded-lg flex items-center gap-4 w">
-				<img src="../assets/icons/basics/trophées.svg" class="w-6" />
-				<p class="font-bold text-white">Trophées</p>
-			</div>
-			<div class="bg-primary p-2 rounded-lg flex items-center gap-4 w">
-				<img src="../assets/icons/basics/shopping.svg" class="w-6" />
-				<p class="font-bold text-white">Shopping</p>
-			</div>
-			<div class="bg-primary p-2 rounded-lg flex items-center gap-4 w">
-				<img src="../assets/icons/basics/réglages.svg" class="w-6" />
-				<p class="font-bold text-white">Réglages</p>
-			</div>
-			<div class="bg-white rounded-lg p-4 mt-2">
-				<p class="font-semibold text-primary">EXP : 0</p>
-				<p class="font-semibold text-primary">Argent : 0 €</p>
-			</div>
+	<div
+		class="hidden h-screen rounded-r-lg bg-dark_grey gap-2 flex-col p-4 w-fit sm:flex"
+	>
+		<div class="justify-center flex">
+			<img
+				src="../assets/icons/devclicker/devclicker-text.svg"
+				class="w-40 md:w-52"
+			/>
 		</div>
+		<h2 class="text-white font-bold mt-2 -mb-2">Junior :</h2>
+		<div class="bg-primary p-2 rounded-lg flex items-center gap-4 w">
+			<img src="../assets/icons/basics/code.svg" class="w-6" />
+			<p class="font-bold text-white">Code</p>
+		</div>
+		<div class="bg-primary p-2 rounded-lg flex items-center gap-4 w">
+			<img src="../assets/icons/basics/missions.svg" class="w-6" />
+			<p class="font-bold text-white">Missions</p>
+		</div>
+		<h2 class="text-white font-bold mt-2 -mb-2">Senior :</h2>
+		<div class="bg-primary p-2 rounded-lg flex items-center gap-4 w">
+			<img src="../assets/icons/basics/investissement.svg" class="w-6" />
+			<p class="font-bold text-white">Investissement</p>
+		</div>
+		<div class="bg-primary p-2 rounded-lg flex items-center gap-4 w">
+			<img src="../assets/icons/basics/entreprise.svg" class="w-6" />
+			<p class="font-bold text-white">Entreprise</p>
+		</div>
+		<div class="bg-primary p-2 rounded-lg flex items-center gap-4 w">
+			<img src="../assets/icons/basics/cybersécurité.svg" class="w-6" />
+			<p class="font-bold text-white">Cybersécurité</p>
+		</div>
+		<h2 class="text-white font-bold mt-2 -mb-2">Autres :</h2>
+		<div class="bg-primary p-2 rounded-lg flex items-center gap-4 w">
+			<img src="../assets/icons/basics/trophées.svg" class="w-6" />
+			<p class="font-bold text-white">Trophées</p>
+		</div>
+		<div class="bg-primary p-2 rounded-lg flex items-center gap-4 w">
+			<img src="../assets/icons/basics/shopping.svg" class="w-6" />
+			<p class="font-bold text-white">Shopping</p>
+		</div>
+		<div class="bg-primary p-2 rounded-lg flex items-center gap-4 w">
+			<img src="../assets/icons/basics/réglages.svg" class="w-6" />
+			<p class="font-bold text-white">Réglages</p>
+		</div>
+		<div class="bg-white rounded-lg p-4 mt-2">
+			<p class="font-semibold text-sm text-primary">EXP : 0</p>
+			<p class="font-semibold text-sm text-primary">Argent : 0 €</p>
+		</div>
+	</div>
 </template>
